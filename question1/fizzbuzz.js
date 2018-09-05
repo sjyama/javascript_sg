a7b276a4a363f3d21e1b2375b2c25098d0b2cb2c
sjyama_mbp <grinpeace101@gmail.com>
2018-09-05 12:58:15 +0900

    eslint, prettierによるフォーマットを実施
    環境設定を行い、自動フォーマットを実施してみました。

--- question1/fizzbuzz.js
+++ question1/fizzbuzz.js
@@ -1,23 +1,19 @@
-'use strict';
+"use strict";
 
-function fizz_buzz(x){
-  if(x % 15 === 0)
-    return "FizzBuzz";
-  else if(x % 3 === 0)
-    return "Fizz";
-  else if(x % 5 === 0)
-    return "Buzz";
-  else
-    return x;
+function fizz_buzz(x) {
+	if (x % 15 === 0) return "FizzBuzz";
+	else if (x % 3 === 0) return "Fizz";
+	else if (x % 5 === 0) return "Buzz";
+	else return x;
 }
 
-function countup(max){
-  var ans = [];
-  for(var n = 1; n < max; n++){
-    ans.push(fizz_buzz(n));
-  }
-  console.log(ans.join(', '));
+function countup(max) {
+	var ans = [];
+	for (var n = 1; n < max; n++) {
+		ans.push(fizz_buzz(n));
+	}
+	console.log(ans.join(", "));
 }
 
-var num = prompt('数字を入力してください');
+var num = prompt("数字を入力してください");
 countup(num);
